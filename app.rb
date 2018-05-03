14da19fccb4e67c65973c0e5f09239ab63b83525
Zac Willmington <zac.willmington@gmail.com>
2018-05-03 22:24:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! I built this!"
   end
 
 end
